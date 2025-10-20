a5f97a895147c669a28a7381f05a3ab534bd3ecb
Sandro-Pimentel <125677890+Sandro-Pimentel@users.noreply.github.com>
2025-10-20 08:47:10 -0300

    [feat]: Created orchestrator agent and restructuring folder architecture

---
+++ app/agents/agent.py
@@ -0,0 +1,60 @@
+from google.adk.agents import LlmAgent
+
+analysis_agent = LlmAgent(
+    model="gemini-2.5-flash",
+    name="nutritional_analysis_agent",
+    description="Analisa a composição nutricional de refeições e dietas.",
+    instruction="""
+        Você é um nutricionista analítico.
+        Analise dietas e refeições, destacando:
+        - calorias totais estimadas
+        - proporção de macronutrientes (carboidrato, proteína, gordura)
+        - adequação aos objetivos (manutenção, perda ou ganho de peso)
+        - adequação as comorbidades presentes na ficha do usuário (diabetes, hipertensão, colesterol alto, sobrepeso)
+        - sugestões de equilíbrio
+        Seu objetivo secundário como agente é responder qualquer dúvida relacionada a nutrição em formato de conversa, priorize respostas de fácil entendimento e informações confiáveis.
+    """,
+)
+
+diet_agent = LlmAgent(
+    model="gemini-2.5-flash",
+    name="diet_recommendation_agent",
+    description="Cria planos alimentares personalizados com base na ficha do usuário.",
+    instruction="""
+        Você é um nutricionista especializado em prescrição alimentar.
+        Gere planos de dieta personalizados com base na ficha do usuário.
+        Considere:
+        - objetivo (perda, ganho, manutenção)
+        - restrições (ex: lactose, glúten, diabetes, hipertensão)
+        - preferências alimentares
+        Formato de retorno: Responda de maneira clara e objetiva, priorizando a atender à requisição do usuário".
+    """,
+)
+
+root_agent = LlmAgent(
+    name="Osquestrador",
+    description="Orquestrador de agentes responsável por definir qual agente usar entre médico e nutricional com base na pergunta do usuário",
+    instruction="""
+        Sua tarefa é direcionar a solicitação do usuário aos Agentes Secundários disponíveis que melhor atende à requisição.
+
+        Fluxo de Trabalho e Processo de Decisão
+        Siga este processo rigoroso em cada interação:
+
+        1.  **Analisar a Solicitação:** Interprete a solicitação do usuário e determine qual o melhor agente para abordá-la.
+
+        2.  **Planejamento e Decomposição (Chain-of-Thought):**
+            * Para cada etapa, identifique o **Agente Secundário** mais adequado.
+            * Formule o **prompt específico e claro** que você enviará a esse Agente.
+
+            3.  **Execução e Orquestração:**
+                * Delegue a tarefa ao Agente Secundário escolhido, fornecendo o prompt específico.
+
+            Agentes e Ferramentas Disponíveis
+            Você pode utilizar os seguintes agentes. Escolha o mais apropriado para a necessidade da etapa:
+
+            * **[diet_recommendation_agent]:** [Cria planos alimentares personalizados com base na ficha do usuário.]
+            * **[nutritional_analysis_agent]: [Analisa a composição nutricional de refeições e dietas.]
+    """,
+    model="gemini-2.5-flash",
+    sub_agents=[diet_agent, analysis_agent]
+)
