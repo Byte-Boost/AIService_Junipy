7d5585560b765947b2281151b3fc48932a15fbc6
Sandro-Pimentel <125677890+Sandro-Pimentel@users.noreply.github.com>
2025-11-04 20:54:14 -0300

    [feat]: Add second example for agent's prompt
    Added a second example to the agent’s prompt to illustrate question clarification.

--- app/agents/diet_agent.py
+++ app/agents/diet_agent.py
@@ -4,7 +4,7 @@ import app.agents.tools as t
 
 diet_agent = LlmAgent(
     model="gemini-2.5-flash",
-    name="diet_recommendation_agent",
+    name="diet_agent",
     description="Cria planos alimentares personalizados com base nos dados do paciente e adiquire esses dados caso necessário.",
     instruction="""
         *Contexto:* Você é um agente nutricional especialista na criação e modificação de dietas personalizadas semanais de segunda a domingo e, se necessário, obter os dados do paciente.
@@ -21,7 +21,7 @@ diet_agent = LlmAgent(
             - Sempre use informações confiáveis sobre dieta, comorbidades e nutrição para o desenvolvimento das dietas.
             - Responda apenas perguntas relacionadas à dieta.
 
-        *Formato de saída:* Para responder o paciente retorne o seguinte padrão:
+        *Formato de saída:* A criação da dieta deve seguir o seguinte padrão:
 
             # DIETA SEMANAL PARA [NOME DO PACIENTE]
 
@@ -44,6 +44,8 @@ diet_agent = LlmAgent(
             | [ALIMENTO 4 E QUANTIDADE] | [ALIMENTO 4 E QUANTIDADE] | [ALIMENTO 4 E QUANTIDADE] | [ALIMENTO 4 E QUANTIDADE] |
 
             ... (Repita o padrão para os dias restantes da semana)
+
+            Para responder dúvidas relacionadas à dieta criada, responda com uma média de 35 palavras.
 
         *Exemplos:*
 
@@ -78,6 +80,18 @@ diet_agent = LlmAgent(
                 | 1 fatia de queijo de minas | Couve-Flor e abobrinha| | |
 
                 ...
+
+            Exemplo 2:
+                [DIETA NO BANCO DE DADOS]
+                Dieta de Sandro
+                    ...
+                    ## Café da manhã
+                    - Opção 1: 1 iogurte desnatado com 1 colher de aveia e 3 morangos
+                    ...
+                
+                Pergunta do usuário: Não gosto de iogurte, o que posso colocar no lugar?
+
+                Resposta: No caso de sua dieta, uma boa alternativa seria a substituição do iogurte por 1/2 xícara de café sem açúcar ou 1/2 xícara de chá.
 
         *Conteúdo de Suporte:* Na necessidade de buscar informações adicionais utilize as seguintes tools:
             - 'search_nutrition_tool' para dados acerca de nutrição;
